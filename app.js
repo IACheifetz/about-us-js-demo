18fa3c14df763edeea133e6560721b95693c532e
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-02 16:07:14 -0800

    added DOM elements from index html to app js file

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // import functions
 import {people} from './data.js';
-console.log(people, 'people');
+//console.log(people, 'people');
 
 // grab DOM elements
-
+const selectEl = document.querySelector('.select');
+const nameEl = document.querySelector('h4');
+const ageEl = document.querySelector('p');
+const hobbiesEl = document.querySelector('ul');
+const bioEl = document.querySelector('span');
+const contain = document.querySelector('div');
+console.log(selectEl, 'names');
+console.log(nameEl, 'names');
+console.log(ageEl, 'names');
+console.log(hobbiesEl, 'names');
+console.log(bioEl, 'names');
+console.log(contain, 'names');
 // set event listeners 
     // get user input
     // use user input to update state 
